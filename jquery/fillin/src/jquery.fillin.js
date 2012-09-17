b77429394dbb75183e84ef14305a7762b39642fe
knurtsysteme <info@knurt.de>
2012-09-17 18:44:18 +0200

    do support more input types

--- jquery/fillin/src/jquery.fillin.js
+++ jquery/fillin/src/jquery.fillin.js
@@ -1,24 +1,49 @@
 /*! serializeObject v 0.00 by http://www.knurtsysteme.de
  * License: MIT License */
-console.error('do not use version 0.00!');
-if (typeof (jQuery) == 'undefined')
+
+if (typeof jQuery == 'undefined')
   throw ("require jQuery");
+
 /**
  * put an object into form fields.
  */
 (function($) {
 
+  /**
+   * control fillining in the form
+   */
   function FillInController(params) {
     var count = 0;
     var val = null;
     function isChecked(value) {
-      return typeof (value) == 'string' && value.length > 0;
+      return typeof value == 'string' && value.length > 0 || typeof value == 'number' && value > 0;
     }
+
+    function getVal(params, key) {
+      var result = params[key];      
+      var tmp = [];
+      if(typeof(result) == 'object') {
+        for ( var innerKey in result) {
+          if (result.hasOwnProperty(innerKey)) {
+            tmp.push(result[innerKey]);
+          }
+        }
+        result = tmp;
+      }
+      return result;
+    }
+
+    /**
+     * fill the given params (see constructor) 
+     * into the form where the name of a form field
+     * is a key of the param and the object value is
+     * the input value.
+     * ignore all other fields (not mentioned in object).
+     */
     this.fillIn = function() {
       for ( var key in params) {
         if (params.hasOwnProperty(key)) {
-          val = params[key];
-          target = $('*[name=' + key + ']');
+          val = getVal(params, key);
           $('input[type=checkbox][name=' + key + ']').each(function(i, e) {
             $(e).attr('checked', isChecked(val));
             count++;
